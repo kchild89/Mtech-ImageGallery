c097ab80aa5aef94115c65663df65a45c61bb020
Kevin Child <kchild89@gmail.com>
2024-12-04 07:34:45 -0700

    Add about me page

---
+++ src/app/about-me/page.js
@@ -0,0 +1,11 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is the About Me page. Welcome!</p>
+    </div>
+  );
+}
