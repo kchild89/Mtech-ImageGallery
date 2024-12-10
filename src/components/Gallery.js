14df9f125e0081c6eea67f78c4a6f24d03bb4c89
Kevin Child <kchild89@gmail.com>
2024-12-10 07:53:26 -0700

    Update to add pagination

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Gallery = ({ selectedCategory }) => {
+const Gallery = ({ selectedCategory, currentPage, imagesPerPage }) => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [filteredImages, setFilteredImages] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
 
-  const apiUrl =
-    "https://api.unsplash.com/photos?client_id=HsUCdPBfsazwvaJ5zMNv6IKGcMWeL_BLxRv61wA5ucQ&per_page=9";
+  const apiUrl = `https://api.unsplash.com/photos?client_id=HsUCdPBfsazwvaJ5zMNv6IKGcMWeL_BLxRv61wA5ucQ&per_page=${imagesPerPage}&page=${currentPage}`;
 
   useEffect(() => {
     const fetchImages = async () => {
       try {
+        setLoading(true);
         const response = await axios.get(apiUrl);
         console.log("API Response:", response.data);
 
@@ -34,7 +33,7 @@ const Gallery = ({ selectedCategory }) => {
     };
 
     fetchImages();
-  }, []);
+  }, [currentPage, imagesPerPage]); // Fetch new images when page or per_page changes
 
   useEffect(() => {
     const filtered =
@@ -54,11 +53,7 @@ const Gallery = ({ selectedCategory }) => {
     <div>
       <div className="gallery grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
         {filteredImages.map((image) => (
-          <div
-            key={image.id}
-            className="image-card cursor-pointer"
-            onClick={() => setSelectedImage(image)}
-          >
+          <div key={image.id} className="image-card cursor-pointer">
             <img
               src={image.urls.small}
               alt={image.alt_description}
@@ -67,24 +62,6 @@ const Gallery = ({ selectedCategory }) => {
           </div>
         ))}
       </div>
-
-      {selectedImage && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50"
-          onClick={() => setSelectedImage(null)}
-        >
-          <div
-            className="modal-content bg-white p-4 rounded-lg max-w-lg max-h-[80vh] overflow-y-auto"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <img
-              src={selectedImage.urls.full}
-              alt={selectedImage.alt_description}
-              className="rounded-lg mb-4"
-            />
-          </div>
-        </div>
-      )}
     </div>
   );
 };
