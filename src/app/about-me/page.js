b9eb542a871946f7ec1af8120ed77a0935f3e4a1
Kevin Child <kchild89@gmail.com>
2024-12-10 07:52:06 -0700

    Update on about me text

--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -5,24 +5,34 @@ export default function AboutMe() {
     <main className="flex-grow px-4 py-8 max-w-3xl mx-auto text-center bg-[var(--background)] text-[var(--foreground)]">
       <h1 className="text-4xl font-bold mb-6">About Me</h1>
       <p className="mb-4">
-        Hi there! My name is <strong>Kevin</strong>, and I’m an aspiring
-        software developer currently diving into the world of coding. My focus
-        includes modern web development with <strong>React</strong> and{" "}
-        <strong>Next.js</strong>.
+        Hello, my name is <strong>Kevin</strong>, and I’m an aspiring software
+        developer passionate about modern web development, particularly with{" "}
+        <strong> JavaScript </strong>
+        and <strong>React</strong>. My journey into coding has been driven by a
+        love for creating seamless and user-focused applications.
       </p>
       <p className="mb-4">
-        One of my favorite projects is a custom image gallery app. It fetches
-        photos from an API, categorizes them, and allows dynamic exploration of
-        filtered images. I’m learning to integrate hooks, reducers, and dynamic
-        rendering to create seamless user experiences.
+        This project is a custom image gallery app designed to simplify the way
+        users explore photos. It fetches images from an API, categorizes them,
+        and provides dynamic filtering options for an intuitive browsing
+        experience. By leveraging React hooks, reducers, and dynamic rendering,
+        I’ve built a platform that prioritizes functionality and smooth
+        navigation.
       </p>
       <p className="mb-4">
-        Beyond coding, I love discovering new technologies and solving creative
-        challenges. Feel free to connect and explore my work!
+        My diverse background as a journeyman electrician with 17 years of
+        high-voltage and industrial experience has instilled in me a
+        detail-oriented and problem-solving mindset. In addition to web
+        development, I’m love the field of pentesting to deepen my understanding
+        of cybersecurity. When I’m not coding, I enjoy playing ice hockey,
+        downhill mountain biking, and spending time with my twin daughters.
+        These interests inspire my creative approach to tackling technical
+        challenges.
       </p>
-      <button className="px-6 py-2 mt-6 rounded text-[var(--foreground)] bg-[var(--background)] border-2 border-transparent hover:border-white transition-colors duration-300">
-        Let’s Connect
-      </button>
+      <p className="mb-4">
+        Feel free to explore this project and connect with me to share feedback
+        or discuss ideas!
+      </p>
     </main>
   );
 }
